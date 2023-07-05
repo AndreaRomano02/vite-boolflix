3e51c37c5ee92a54ce4def40af37de573b7699bc
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-05 12:49:05 +0200

    Get request test movies with axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <script>
 // Import API key
 import { api_key } from './assets/data/api_key'
+import { baseUri } from './assets/data/data'
 
 // Import axios Library
 import axios from 'axios'
+const axiosParams = {
+  params: {
+    api_key,
+  }
+};
 
 export default {
   data() {
     return {
       api_key,
+      baseUri,
     }
-  }
+  },
+  methods: {
+    fetchMovies(uri = this.baseUri) {
+      axios.get(uri + `/search/movie`, axiosParams)
+        .then(res => {
+          console.log(res.data)
+
+        })
+    },
+  },
+  created() {
+    this.fetchMovies();
+  },
 }
 </script>
 

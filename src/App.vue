18119751107da2c98a9de7df773afb1303dfdbe4
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-05 12:28:05 +0200

    Add api_key from data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
-export default {
+import { api_key } from './assets/data/api_key'
 
+export default {
+  data() {
+    return {
+      api_key,
+    }
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <h1>{{ api_key }}</h1>
+</template>
 
 <style></style>
---
+++ src/assets/data/api_key.js
@@ -0,0 +1,1 @@
+export const api_key = '5bf8d307e9b784203ef1907a3b9752bd';
